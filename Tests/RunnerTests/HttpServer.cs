2495afad0d7cf9cfa46e14dc223452797f58c8ae
georgebearden <george@goerden.com>
2015-08-06 13:33:40 -0400

    Added an example implementation of the Router class.

---
+++ Tests/RunnerTests/HttpServer.cs
@@ -0,0 +1,46 @@
+﻿using HttpRouter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RunnerTests
+{
+  public class HttpServer : IDisposable
+  {
+    private readonly HttpListener _server;
+    private readonly Task _serverTask;
+    private readonly CancellationTokenSource _cancelTokenSource;
+
+    public HttpServer(Router router)
+    {
+      _cancelTokenSource = new CancellationTokenSource();
+
+      _server = new HttpListener();
+      _server.Prefixes.Add(@"http://localhost:5556/");
+      _server.Start();
+
+      _serverTask = Task.Run(async () =>
+      {
+        while (!_cancelTokenSource.IsCancellationRequested)
+        {
+          var context = await Task.Run(() => _server.GetContext(), _cancelTokenSource.Token);
+
+          var endpoint = context.Request.Url.LocalPath;
+          router.HandleRequest(endpoint, context);
+        }
+      }, _cancelTokenSource.Token);
+    }
+
+    public void Dispose()
+    {
+      if (_cancelTokenSource.IsCancellationRequested)
+        return;
+
+      _cancelTokenSource.Cancel();
+    }
+  }
+}
--- Tests/RunnerTests/Program.cs
+++ Tests/RunnerTests/Program.cs
@@ -1,15 +1,42 @@
-﻿using System;
+﻿using HttpRouter;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RunnerTests
 {
-  class Program
+  public class Program
   {
-    static void Main(string[] args)
+    public static void Callback(RoutedHttpRequest request, HttpListenerResponse response)
     {
+      Console.WriteLine(request.Request.Url);
+      foreach (var param in request.Params)
+      {
+        Console.WriteLine("{0}={1}", param.Key, param.Value);
+      }
+      response.StatusCode = 200;
+      response.OutputStream.Close();
+    }
+
+    public static void Main(string[] args)
+    {
+      var router = new Router();
+      router.Route(HttpMethods.Get, "/entities", Callback);
+      router.Route(HttpMethods.Get, "/entities/", Callback);
+      router.Route(HttpMethods.Get, "/entities/{userId}", Callback);
+      router.Route(HttpMethods.Get, "/entities/{userId}/", Callback);
+      router.Route(HttpMethods.Get, "/entities/{userId}/names", Callback);
+      router.Route(HttpMethods.Get, "/entities/{userId}/names/", Callback);
+      router.Route(HttpMethods.Get, "/entities/{userId}/names/{nameId}", Callback);
+      router.Route(HttpMethods.Get, "/entities/{userId}/names/{nameId}/", Callback);
+
+      using (var httpServer = new HttpServer(router))
+      {
+        Console.ReadLine();
+      }
     }
   }
 }

5550f29adbbe47c4dd15c138b2fee42a195ee1df
George Bearden <george@goerden.com>
2015-11-19 21:59:00 -0500

    More name updates.

--- Tests/Expresso.Net45.Tests/Program.cs
+++ Tests/Expresso.Net45.Tests/Program.cs
@@ -1,12 +1,8 @@
-﻿using HttpRouter;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using Expresso;
 
-namespace RunnerTests
+namespace Expresso
 {
   public class Program
   {
